0d009b234ed9df4f51f8b5411738c8e3faa3526f
Prenapat <73680684+Prenapat@users.noreply.github.com>
2020-11-06 17:57:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,53 @@
-<?php
-echo "<h2>PHP is Fun!</h2>";
-echo "Hello world!<br>";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Excercise2</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+
+    <style>
+        body{
+            background-color: lightblue;
+        }
+        
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="card">
+                <img class="card-img-top" src="img/life/life.jpg">
+                    <div class="card-body">
+                        <h4 class="card-title">Our Staff</h4>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur !<p>
+                        <a href="#" class="btn btn-primary">Read more</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card">
+                <img class="card-img-top" src="img/life/life4.jpg">
+                    <div class="card-body">
+                        <h4 class="card-title">Our Staff</h4>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur !<p>
+                        <a href="#" class="btn btn-primary">Read more</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card">
+                <img class="card-img-top" src="img/life/life9.jpg">
+                    <div class="card-body">
+                        <h4 class="card-title">Our Staff</h4>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur !<p>
+                        <a href="#" class="btn btn-primary">Read more</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
